a16fa654d7358a6994cf647936e29430ab5f9e6f
deepthi1219 <kdeepthi071@gmail.com>
2017-10-19 01:20:07 -0500

    t12

--- demo.java
+++ demo.java
@@ -4,7 +4,7 @@ class demo{
 
 public static void main(String args[]){
 int num=10;
-System.out.println("Print number display Number" + num);
+System.out.println("Print number display Number test prog" + num);
 
 System.out.println("total is" + num);
 }
